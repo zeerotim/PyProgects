b9ff6690566407bd46306817729175a14ac56b66
zeerotim <timurrg@gmail.com>
2021-12-26 14:39:25 +0400

    5 задание урок 2

--- lesson02_hw05.py
+++ lesson02_hw05.py
@@ -4,6 +4,25 @@
 # то новый элемент с тем же значением должен разместиться после них.
 
 my_list = [7, 5, 3, 3, 2]
-
+my_list.sort(reverse = True) # на всякий случай отсортируем по-убыванию, вдруг изначально не отсортирован
 new_el = int(input("Введите новый элемент списка: "))
 
+if new_el in my_list:
+    ind = my_list.index(new_el)
+    my_list.insert(ind, new_el)
+else:
+    max_value = my_list[0]
+    min_value = my_list[len(my_list)-1]
+    if new_el > max_value:
+        my_list.insert(0, new_el)
+    elif new_el < min_value:
+        my_list.append(new_el)
+    else:
+        ind = 0
+        while new_el < my_list[ind]:
+            ind += 1
+        my_list.insert(ind, new_el)
+# Можно было вообще не париться с кучей уcловий, а просто добавить новый элемент в список (append)
+# и отсортировать по убыванию :)
+
+print(my_list)
