e2c8489d93e44335b383d21ad472e87b4515ce84
zeerotim <timurrg@gmail.com>
2021-12-30 00:13:01 +0400

    4 задание урок 3

--- lesson03_hw04.py
+++ lesson03_hw04.py
@@ -1,13 +1,9 @@
 def my_func1(x:float, y:int):
-    if x == 0:
-        return 0
-    result = 1
-    if y != 0:
-       result = x**y
+    result = x**y
     return result
 
 def my_func2(x:float, y:int):
-    if x == 0:
+    if x == 0 and y != 0:
         return 0
     result = 1
     if y > 0:
@@ -18,6 +14,6 @@ def my_func2(x:float, y:int):
             result = result/x
     return result
 
-print(my_func1(2, -3))
+print(my_func1(0, 0))
 
-print(my_func2(2, -3))
+print(my_func2(0, 0))
